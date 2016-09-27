7f7b4580992b2f878429be8de527e7275067f709
Paddy <p.withams@gmail.com>
2016-09-26 18:30:10 -0600

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -1,9 +1,13 @@
 install_path=/usr/local/bin
+mkdir crypto_download_scripts
+cd crypto_download_scripts
 echo "Downloading Crypto..."
 wget -q https://raw.githubusercontent.com/paddyw2/crypto/master/crypto
 wget -q https://raw.githubusercontent.com/paddyw2/crypto/master/dcrypto
 echo "Installing Crypto..."
 chmod +x crypto && mv crypto $install_path
 chmod +x dcrypto && mv dcrypto $install_path
+cd ..
+rm -rf crypto_download_scripts
 echo "Installation finished"
 echo "Type 'crypto <target directory>' to encrypt a folder"
